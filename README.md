b976d47323b633cd271887ead798facaa5b7f910
randomSmarts <67401115+randomSmarts@users.noreply.github.com>
2026-06-29 14:10:48 -0700

    Updated README.md w/Wi-Fi types; Implemented v1 of connecting to Wi-Fi on Linux by QR code, need to test;

--- README.md
+++ README.md
@@ -73,3 +73,39 @@
   - Use for high-quality editing or scientific/medical images
 - Rgb32F / Rgba32F
   - Usually not needed unless doing advanced image processing
+
+## Wi-Fi Connect
+
+### WPA Style:
+
+- `WIFI:T:WPA;S:MyNetworkName;P:MyPassword;;`
+- Solution: Can do basic string parsing to read data --> connect to Wi-Fi
+
+### Open Wi-Fi w/Captive Portal
+
+* `WIFI:S:Venue_Free_WiFi;T:nopass;P:;;`
+* Connects device to open network, but internet access is blocked until a web/captive portal is completed
+* If robot reboots / Wi-Fi lease expires, device will be stuck w/o actual internet
+* Solution: Whitelist robot's MAC address beforehand so no captive portals arrive
+
+### Standard URL
+
+* QR code points to standard website
+* Read instructions as to how to connect to Wi-Fi
+* Main issue here
+
+### WPA-Enterprise / Enrollment URL
+
+* Every single device gets its own unique, temporary credentials/digital certificate
+* URL takes the device's browser to a secrue provisioning server
+* Server looks at the device and generates a customized network profile (config. file w/security certificates and specific enterprise Wi-Fi settings)
+* For unattended devcies can connect beforehand using Mobile Device Management (MDM) software
+
+### Peplink
+
+* Check if router has dual-band, simultaneous Wi-Fi radios
+* Local AP connects all devices to router
+* Only need to point router to a Wi-Fi WAN
+* When connecting to a Wi-Fi with a captive portal, either use the `Captive Portal Sign-On` feature
+  * Or connect a device to the Peplink local AP, try to go to the venue's website / any website, venue's system should intercept it and serve the Captive Portal splash page on the device itself, once you are online, the venue's system associates the confirmation with the Peplink’s MAC address and so now every device connected to your Peplink has full internet access
+  * For WPA-Enterprise, change the security type, enter the Username and Password, and if need be connect a device to local AP and visit the required website to get the unique 802.1X wireless username and password credentials
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use std::{ io };
 
-use nokhwa::{ Camera, pixel_format::{LumaFormat}, utils::{ CameraIndex, RequestedFormat, RequestedFormatType }
+use regex::Regex;
+use std::sync::LazyLock; // from Rust standard library; lets you create something globally, only initializing it the first time it is used
+use std::process::Command; // Lets Rust run system commands
+
+use nokhwa::{
+    Camera,
+    pixel_format::{ LumaFormat },
+    utils::{ CameraIndex, RequestedFormat, RequestedFormatType },
 };
 
 use zedbar::config::*; // Bring everything public from zedbar::config into scope
 use zedbar::{ DecoderConfig, Scanner };
+
+static WIFI_REGEX: LazyLock<Regex> = LazyLock::new(|| {
+    // || {} is a closure that stores the code that runs once (an anonymous
+    // function of sorts)
+    Regex::new(
+        r"^WIFI:T:(?P<wifi_type>[^;]+);S:(?P<wifi_ssid>[^;]+);P:(?P<wifi_pwd>[^;]*);.*$" // + means ≥1 chars, * means ≥1 chars
+    ).unwrap()
+}); // poisoning is where when you create a shared resource, it runs some code inside LazyLock::new the first time it is accessed; if that code panics, then it poisons the the lock and any future attempt to use the data in the lock will panic and crash the program
 
 fn main() {
     let mut user_input: String = String::new();
@@ -12,7 +27,8 @@ fn main() {
     let index = CameraIndex::Index(0); // CameraIndex is a type, and so using CameraIndex::Index(0) means ot use camera number 0
 
     let requested = RequestedFormat::new::<LumaFormat>(
-        // creates a camera format request, uses turbofish syntax telling rust call new and use RgbFormat as the format type with highest frame rate
+        // creates a camera format request, uses turbofish syntax telling rust
+        // call new and use RgbFormat as the format type with highest frame rate
         RequestedFormatType::AbsoluteHighestFrameRate
     );
 
@@ -24,10 +40,10 @@ fn main() {
     warm_up_camera(&mut camera); // removing unnecessary flame flushing from hot path because camera acquisition latency dominates QR decode latency
 
     let config = DecoderConfig::new() // settings object
-            .enable(QrCode) // tell scanner to look for QR codes
-            .test_inverted(true) // if normal scan finds nothing, try scanning an inverted image (white squares on black background)
-            .retry_undecoded_regions(true) // If zedbar sees QR finder patterns but can't decode the QR, auto crop/upscale the sus regions and try again
-            .scan_density(2, 2); // density control, about scanning every x rows and every y columns
+        .enable(QrCode) // tell scanner to look for QR codes
+        .test_inverted(true) // if normal scan finds nothing, try scanning an inverted image (white squares on black background)
+        .retry_undecoded_regions(true) // If zedbar sees QR finder patterns but can't decode the QR, auto crop/upscale the sus regions and try again
+        .scan_density(2, 2); // density control, about scanning every x rows and every y columns
 
     let mut scanner = Scanner::with_config(config);
 
@@ -47,13 +63,23 @@ fn main() {
             println!("Thank you for using this service. Good bye.");
             break;
         } else if command == "c" {
-            println!("The data in the QR code is: {}", fallback_detect_qr_code_loop(&mut camera, &mut scanner));
+            let data_result = fallback_detect_qr_code_loop(&mut camera, &mut scanner);
+            let connector = LinuxNetworkManagerConnector;
+            let result = connect_to_wifi_from_qr_payloads(&data_result, &connector);
+            if result.is_ok() {
+                println!("Connected to Wi-Fi successfully.");
+            } else {
+                println!("Failed to connect to Wi-Fi: {:?}", result.err());
+            }
+            // println!("The data in the QR code is: {}",
+            // fallback_detect_qr_code_loop(&mut camera, &mut scanner));
         }
     }
 }
 
-fn fallback_detect_qr_code_loop(camera: &mut Camera, scanner: &mut Scanner) -> String {
-    loop {
+fn fallback_detect_qr_code_loop(camera: &mut Camera, scanner: &mut Scanner) -> Vec<String> {
+    let mut qr_code_data: Vec<String> = Vec::new();
+    while qr_code_data.is_empty() {
         let frame = camera.frame().expect("Failed to capture frame.");
         let decoded_frame = frame.decode_image::<LumaFormat>().expect("Failed to decode frame.");
 
@@ -69,14 +95,174 @@ fn fallback_detect_qr_code_loop(camera: &mut Camera, scanner: &mut Scanner) -> S
         for symbol in symbols {
             let data = symbol.data_string().unwrap_or("");
             if !data.is_empty() {
-                return data.to_string();
+                qr_code_data.push(data.to_string());
             }
         }
     }
+    qr_code_data
 }
 
 fn warm_up_camera(camera: &mut Camera) {
     for _ in 0..5 {
         let _ = camera.frame();
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)] // generates some common behavior for this type
+enum WifiSecurity {
+    Wpa,
+    Wep,
+    Open,
+}
+
+#[derive(Debug)]
+enum WifiConnectError {
+    MissingPassword,
+    UnsupportedSecurity,
+    CommandFailed(String),
+    Io(std::io::Error),
+    NoCredentialsFound,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct WifiCredentials {
+    security_type: WifiSecurity,
+    ssid: String,
+    password: Option<String>,
+}
+
+fn parse_wifi_credentials(qr_payloads: &[String]) -> Option<WifiCredentials> {
+    // reference to a slice of strings, don't care what container they came from
+    // WIFI:T:WPA;S:MyNetworkName;P:MyPassword;;
+
+    for payload in qr_payloads {
+        if let Some(caps) = WIFI_REGEX.captures(payload) {
+            // Rust auto converts &String to &str
+            let ssid = caps["wifi_ssid"].to_string();
+
+            let raw_security_type = caps["wifi_type"].to_string(); // &caps["wifi_type"].to_string(); means to create a new string and then borrow it
+            let security_type = match raw_security_type.as_str() {
+                "WPA" | "WPA2" | "WPA3" => WifiSecurity::Wpa,
+                "WEP" => WifiSecurity::Wep,
+                "nopass" | "" => WifiSecurity::Open,
+                _ => {
+                    // anything else to just continue to next payload
+                    continue;
+                }
+            };
+
+            let password_text = caps // password is optional
+                .name("wifi_pwd") // try to get the named capture group, returning an Option
+                .map(|m| m.as_str().to_string()) // tiny closure function, converts whatever is inside the Option to a string (if not None)
+                .unwrap_or_default(); // returns string or default which is String::new() / ""
+            let password = match security_type {
+                // Converts password back into an Option, also for checking where if WifiSecurity is given but no password then go to next payload
+                WifiSecurity::Wpa | WifiSecurity::Wep => {
+                    if password_text.is_empty() {
+                        continue;
+                    }
+                    Some(password_text)
+                }
+                WifiSecurity::Open => None,
+            };
+
+            return Some(WifiCredentials { security_type, ssid, password });
+        }
+    }
+    None
+}
+
+fn connect_to_wifi_from_qr_payloads(
+    qr_payloads: &[String],
+    connector: &impl WifiConnector
+) -> Result<(), WifiConnectError> {
+    // &impl WifiConnector means give me a reference to any type that implements
+    // WifiConnector (abstracting away what exact connector is being passed in)
+    let credentials = parse_wifi_credentials(qr_payloads).ok_or(
+        WifiConnectError::NoCredentialsFound
+    )?;
+    connector.connect(&credentials)
+    // ? means if it is an error, return the error from the curr. function
+    // immediately; works for Result or Option; can only use it if this function
+    // itself returns a valid Result or Option because there's always a chance
+    // it needs to return an error
+
+    // .ok_or converts option to result
+}
+
+trait WifiConnector {
+    fn connect(&self, credentials: &WifiCredentials) -> Result<(), WifiConnectError>;
+}
+// trait = promise of behavior; any type that wants to be called WifiConnector
+// must implement connect function; also could be like a shared interface of
+// sorts where you could have a bunch of different connectors or functions that
+// all have connect, so this shared interface makes it easier traits are used
+// because want a high-level connector.connect(), not a massive if-else
+// statement two types of impl, one is to add methods directly to a type, other
+// is a trait impl, teaching a type how to satisfy a trait
+
+struct LinuxNetworkManagerConnector;
+// interface_name: String,
+// unit struct, needed when you only need a type to attach behavior to
+
+impl LinuxNetworkManagerConnector {
+    fn new(interface_name: String) -> Self {
+        LinuxNetworkManagerConnector {}
+    }
+}
+
+impl WifiConnector for LinuxNetworkManagerConnector {
+    fn connect(&self, credentials: &WifiCredentials) -> Result<(), WifiConnectError> {
+        match credentials.security_type {
+            WifiSecurity::Wpa => {
+                let password = credentials.password
+                    .as_ref() // we do .as_ref() for all because credentials, where the password comes from, is borrowed
+                    .ok_or(WifiConnectError::MissingPassword)?;
+                let output = Command::new("nmcli")
+                    .args([
+                        "device",
+                        "wifi",
+                        "connect",
+                        credentials.ssid.as_str(), // as_str() because that's how the terminal likes it
+                        "password",
+                        password.as_str(),
+                    ])
+                    .output()?; // ? used because it returns Result, where if the command failed then it returns the error immediately
+
+                if output.status.success() {
+                    Ok(())
+                } else {
+                    return Err(
+                        WifiConnectError::CommandFailed(
+                            String::from_utf8_lossy(&output.stderr).to_string() // output.stderr is raw bytes (Vec<u8>) so this says to try to interpret these bytes as UTF-8 text and if some fail then replace them with a safe replacement character, returning Cow<str> and finally an owned String
+                        )
+                    );
+                }
+            }
+            WifiSecurity::Wep => { Err(WifiConnectError::UnsupportedSecurity) }
+            WifiSecurity::Open => {
+                let output = Command::new("nmcli")
+                    .args(["device", "wifi", "connect", credentials.ssid.as_str()])
+                    .output()?;
+
+                if output.status.success() {
+                    Ok(())
+                } else {
+                    Err(
+                        WifiConnectError::CommandFailed(
+                            String::from_utf8_lossy(&output.stderr).to_string()
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+impl From<std::io::Error> for WifiConnectError {
+    // converting from one to another since connect returns WifiConnectError, so
+    // .into() is auto. available then (the conversion ? uses)
+    fn from(error: std::io::Error) -> Self {
+        WifiConnectError::Io(error)
     }
 }
