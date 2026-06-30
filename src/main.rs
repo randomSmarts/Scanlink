3b511f254fa85d563d2c5f9b24f3092639d414a8
Aarsh Mittal <aarshsmittal@gmail.com>
2026-06-29 17:05:07 -0700

    Fixed WifiSecurity::Open; Added comments; QOL Formatting;

--- src/main.rs
+++ src/main.rs
@@ -254,46 +254,39 @@ impl WifiConnector for LinuxNetworkManagerConnector {
                     .as_ref() // we do .as_ref() for all because credentials, where the password comes from, is borrowed
                     .ok_or(WifiConnectError::MissingPassword)?;
 
-                let delete_existing = Command::new("nmcli")
-                    .args([
-                        "connection",
-                        "delete",
-                        credentials.ssid.as_str(),
-                    ])
+                let _ = Command::new("nmcli") // deletes old broken profiles with the corresponding SSID name, don't care about errors
+                    .args(["connection", "delete", credentials.ssid.as_str()])
                     .output();
 
-                let _ = delete_existing; // ignore delete failure because profile may not exist yet
-                let add_output = Command::new("nmcli")
+                let add_output = Command::new("nmcli") // create a proper WPA profile
                     .args([
                         "connection",
                         "add",
-                        "type",
+                        "type", // wifi connection this is
                         "wifi",
-                        "ifname",
+                        "ifname", // use any available wi-fi interface
                         "*",
-                        "con-name",
+                        "con-name", // name saved connection profile after the SSID
                         credentials.ssid.as_str(), // as_str() because that's how the terminal likes it
-                        "ssid",
+                        "ssid", // actual wi-fi SSID name to connect to
                         credentials.ssid.as_str(),
-                        "wifi-sec.key-mgmt",
+                        "wifi-sec.key-mgmt", // these are the missing settings (this and below)
                         "wpa-psk",
-                        "wifi-sec.psk",
+                        "wifi-sec.psk", // This is WPA/WPA-2 Personal using a pre-shared key
                         password.as_str(),
                     ])
                     .output()?; // ? used because it returns Result, where if the command failed then it returns the error immediately
 
                 if !add_output.status.success() {
-                    return Err(WifiConnectError::CommandFailed(
-                        String::from_utf8_lossy(&add_output.stderr).to_string(),
-                    ));
+                    return Err(
+                        WifiConnectError::CommandFailed(
+                            String::from_utf8_lossy(&add_output.stderr).to_string()
+                        )
+                    );
                 }
 
-                let up_output = Command::new("nmcli")
-                    .args([
-                        "connection",
-                        "up",
-                        credentials.ssid.as_str(),
-                    ])
+                let up_output = Command::new("nmcli") // connect the profile just created (activating it)
+                    .args(["connection", "up", credentials.ssid.as_str()])
                     .output()?;
 
                 if up_output.status.success() {
@@ -308,16 +301,43 @@ impl WifiConnector for LinuxNetworkManagerConnector {
             }
             WifiSecurity::Wep => { Err(WifiConnectError::UnsupportedSecurity) }
             WifiSecurity::Open => {
-                let output = Command::new("nmcli")
-                    .args(["device", "wifi", "connect", credentials.ssid.as_str()])
+                let _ = Command::new("nmcli")
+                    .args(["connection", "delete", credentials.ssid.as_str()])
+                    .output();
+
+                let add_output = Command::new("nmcli")
+                    .args([
+                        "connection",
+                        "add",
+                        "type",
+                        "wifi",
+                        "ifname",
+                        "*",
+                        "con-name",
+                        credentials.ssid.as_str(),
+                        "ssid",
+                        credentials.ssid.as_str(),
+                    ])
                     .output()?;
 
-                if output.status.success() {
+                if !add_output.status.success() {
+                    return Err(
+                        WifiConnectError::CommandFailed(
+                            String::from_utf8_lossy(&add_output.stderr).to_string()
+                        )
+                    );
+                }
+
+                let up_output = Command::new("nmcli")
+                    .args(["connection", "up", credentials.ssid.as_str()])
+                    .output()?;
+
+                if up_output.status.success() {
                     Ok(())
                 } else {
                     Err(
                         WifiConnectError::CommandFailed(
-                            String::from_utf8_lossy(&output.stderr).to_string()
+                            String::from_utf8_lossy(&up_output.stderr).to_string()
                         )
                     )
                 }
